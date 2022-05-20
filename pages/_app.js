e123c291da3498161658512e10a22e629a8d9d20
Your Name <you@example.com>
2022-05-19 21:02:30 -0400

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <div>
+    <Head>
+        <link rel="favicon" href="favicon.ico" />
+        <title>McNair Student Main</title>
+    </Head>
+
+    {/* <p> </p> */}
+    
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
